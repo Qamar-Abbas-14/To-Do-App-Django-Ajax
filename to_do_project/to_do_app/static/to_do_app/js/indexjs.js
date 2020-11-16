cb8009ea021bc58293f489e79e037eb543042695
unknown <qamar.abbas14@hotmail.com>
2020-11-16 23:05:13 +0500

    Added js files- page styling

---
+++ to_do_project/to_do_app/static/to_do_app/js/indexjs.js
@@ -0,0 +1,14 @@
+
+
+$( document ).ready(function() {
+    console.log("Document is ready");
+    // $("#textbox-1").
+
+    $( "#btn-2" ).click(function() {
+        // alert( "Handler for .click() called." );
+        var value=$("#textbox-1").val();
+        alert(value);
+    });
+ 
+});
+
