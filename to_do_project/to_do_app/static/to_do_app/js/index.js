60278e3ff5008be1a9ed9ad97e24030b66a65e46
unknown <qamar.abbas14@hotmail.com>
2020-11-19 23:06:55 +0500

    added card in index

---
+++ to_do_project/to_do_app/static/to_do_app/js/index.js
@@ -0,0 +1,26 @@
+
+
+$( document ).ready(function() {
+    
+    // $("#textbox-1").
+
+    $( "#btn-2" ).click(function() {
+        // function for getting caurrent value in text box
+        var value=$("#textbox-1").val();
+        alert(value);
+    });
+    $( "#btn-3" ).click(function() {
+        console.log("Button Clicked");
+        
+        // function for getting caurrent value in text box
+        var value=$(".card-body");
+        console.log(value);
+        console.log(value.text());
+    });
+
+    
+ 
+});
+
+
+
