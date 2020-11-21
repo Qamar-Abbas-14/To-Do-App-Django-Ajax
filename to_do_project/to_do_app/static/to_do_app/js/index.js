3c23af11c2d0c6e0025f252ff08fd86cf0af7ca6
unknown <qamar.abbas14@hotmail.com>
2020-11-21 22:53:26 +0500

    mul card on click btn

--- to_do_project/to_do_app/static/to_do_app/js/index.js
+++ to_do_project/to_do_app/static/to_do_app/js/index.js
@@ -6,11 +6,21 @@ $( document ).ready(function() {
 
     $( "#btn-2" ).click(function() {
         // function for getting caurrent value in text box
-        var value=$("#textbox-1").val();
-        alert(value);
+        // var value=$("#textbox-1").val();
+        // var cardcontainer=$(".card-container");
+        // var content="<p>Card Content</p>"
+        // cardcontainer.append(content);
+        // alert(value);
+        console.log("Button Clicked");
+        var textboxcontent=$('#textbox-1').val();
+        var cardcontainer=$(".card-container");
+        var content="<div class='card mb-1' id='task-card-id'><div class='card-body'>"
+        content +="<p>"+textboxcontent+"</p>";
+        content += "<button type='button' id='btn-3' class='close'><span aria-hidden='true'>&times;</span></button></div></div>";
+
+        cardcontainer.append(content);
     });
     $( "#btn-3" ).click(function() {
-        console.log("Button Clicked");
         
         // function for getting caurrent value in text box
         var value=$(".card-body");
@@ -18,7 +28,7 @@ $( document ).ready(function() {
         console.log(value.text());
         var elem= $("#task-card-id")
         console.log(elem);
-        elem.remove();
+        elem.remove()
     });
 
     
