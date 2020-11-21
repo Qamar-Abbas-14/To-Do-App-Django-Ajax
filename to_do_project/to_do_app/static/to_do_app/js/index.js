e48531b830aafd6eb592d485c1434244e179f052
unknown <qamar.abbas14@hotmail.com>
2020-11-21 23:31:12 +0500

    eventhandler in js

--- to_do_project/to_do_app/static/to_do_app/js/index.js
+++ to_do_project/to_do_app/static/to_do_app/js/index.js
@@ -4,32 +4,38 @@ $( document ).ready(function() {
     
     // $("#textbox-1").
 
-    $( "#btn-2" ).click(function() {
+    $( "#create-btn" ).click(function() {
         // function for getting caurrent value in text box
         // var value=$("#textbox-1").val();
         // var cardcontainer=$(".card-container");
         // var content="<p>Card Content</p>"
         // cardcontainer.append(content);
         // alert(value);
-        console.log("Button Clicked");
+        
         var textboxcontent=$('#textbox-1').val();
         var cardcontainer=$(".card-container");
         var content="<div class='card mb-1' id='task-card-id'><div class='card-body'>"
         content +="<p>"+textboxcontent+"</p>";
-        content += "<button type='button' id='btn-3' class='close'><span aria-hidden='true'>&times;</span></button></div></div>";
+        content += "<button type='button' id='btnn-3' class='close'><span aria-hidden='true'>&times;</span></button></div></div>";
 
         cardcontainer.append(content);
     });
-    $( "#btn-3" ).click(function() {
+    // $( "#btn-3" ).click(function() {
         
-        // function for getting caurrent value in text box
-        var value=$(".card-body");
-        console.log(value);
-        console.log(value.text());
-        var elem= $("#task-card-id")
-        console.log(elem);
-        elem.remove()
-    });
+    //     // function for getting caurrent value in text box
+    //     var value=$(".card-body");
+    //     console.log(value);
+    //     console.log(value.text());
+    //     var elem= $("#task-card-id")
+    //     console.log(elem);
+    //     elem.remove()
+    // });
+    // $('button[id^="btn-"]').on('click', function() {  
+    //     alert("nnnn");
+    //  });
+     $(document).on("click","button[id^='btnn-']",function(){
+        alert(this.id);
+      });
 
     
  
